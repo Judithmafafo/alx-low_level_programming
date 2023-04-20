3ecd4b03a3102b74033176524f6d1c3d5c4fe8ac
Judithmafafo <mafafojudith22@gmail.com>
2023-04-20 15:23:00 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,5 +5,8 @@
  * Return: Always 0 (success)
  */
 int main(void)
-	puts("\"Programming is like building a multilingual puzzle");
-	Return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return (0);
+}
+
