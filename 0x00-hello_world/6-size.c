488686d7e012847ca5b6be492a78661196926d6f
Judithmafafo <mafafojudith22@gmail.com>
2023-04-24 16:02:27 -0700

    betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ printf("size of an int: %4d byte(s)\n", sizeof(int));
 printf("size of a long int: %4d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %8d byte(s)\n", sizeof(long long int));
 printf("size of a float: %4d byte(s)\n", sizeof(float));
-return (0)
+return (0);
 }
 
 
